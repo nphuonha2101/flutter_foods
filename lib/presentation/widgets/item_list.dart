245a525b038994ba0cf5090ca1b6bdd44af929a4
Phuong Nha Nguyen <nphuonha2101@gmail.com>
2024-10-10 22:53:48 +0700

    #8 - feat: create food card

---
+++ lib/presentation/widgets/item_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_foods/presentation/widgets/food_card.dart';
+
+class ItemGrid extends StatelessWidget {
+  // final List<Food> foods;
+
+  // const ItemGrid({super.key, this.foods});
+
+  const ItemGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return ListView.builder(
+    //   itemBuilder: (context, index) => {
+    //     // return FoodCard({super.key,foods[index]})
+    //   },
+    // );
+
+    throw UnimplementedError();
+  }
+}
