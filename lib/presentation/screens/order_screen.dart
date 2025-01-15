b77dc1aa6b9c803ca43468736ff05944c564bc8c
=Dat <phamledatctbp@gmail.com>
2025-01-15 10:49:54 +0700

    update order

--- lib/presentation/screens/order_screen.dart
+++ lib/presentation/screens/order_screen.dart
@@ -1,13 +1,16 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_foods/core/constants/payment_method.dart';
+import 'package:flutter_foods/core/routes/app_routes.dart';
 import 'package:flutter_foods/data/models/address.dart';
 import 'package:flutter_foods/data/models/cart_item.dart';
 import 'package:flutter_foods/data/models/order_item.dart';
-import 'package:flutter_foods/data/models/user.dart';
 import 'package:flutter_foods/presentation/widgets/order_screen_bottom_app_bar.dart';
 import 'package:flutter_foods/providers/cart_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_foods/data/models/food_cart_item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -15,59 +18,51 @@ class OrderScreen extends StatefulWidget {
   @override
   State<OrderScreen> createState() => _OrderScreenState();
 }
-
 class _OrderScreenState extends State<OrderScreen> {
   int _paymentMethod = -1;
   String note = '';
   late Map<int, List<OrderItem>> itemsByIdShops;
   int id_address = 1;
-
-  final User user = User(
-    id: 1,
-    name: 'Nguyễn Văn A',
-    email: 'email@example.com',
-    phone: '0123456789',
-    address: '123 Đường ABC, Quận XYZ, TP. HCM',
-    avatarUrl:
-        'https://th.bing.com/th/id/OIP.w8GMRtBU7GOWXzf6ebKGJwHaHa?rs=1&pid=ImgDetMain',
-    createdAt: DateTime.now(),
-    updatedAt: DateTime.now(),
-  );
-
-  final Address primaryAddress = Address(
-    id: 1,
-    name: 'Nguyễn Văn A',
-    address: '123 Đường ABC, Quận XYZ, TP. HCM',
-    phone: '0123456789',
-    isDefault: false,
-    userId: 1,
-  );
+  late List<CartItem> cartItems = [];
+  late SharedPreferences prefs;
+  Address? primaryAddress;
 
   @override
   void initState() {
     super.initState();
+    initSharedPreferences();
+  }
 
-    List<OrderItem> orderItems = [];
+  // Async method to initialize SharedPreferences
+  Future<void> initSharedPreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    String? primaryAddressJson = prefs.getString('defaultAddress');
+    if (primaryAddressJson != null) {
+      setState(() {
+        primaryAddress = Address.fromJsonStatic(jsonDecode(primaryAddressJson)) as Address?;
+      });
+    }
+
     List<int> id_shops = [];
-    itemsByIdShops = {}; // Map<int, List<OrderItem>>
+    itemsByIdShops = {};
 
-    List<CartItem> cartItems = Provider.of<CartProvider>(context, listen: false).getCartItems();
-    for (CartItem cartItem in cartItems){
-       for (FoodCartItem foodsShopItem in cartItem.items){
-            OrderItem orderItem = OrderItem(
-                  foodId: foodsShopItem.food.id,
-                  quantity: foodsShopItem.quantity,
-                  price: foodsShopItem.food.price,
-                  id: 0,
-            );
-            if (!id_shops.contains(foodsShopItem.food.shopId)) {
-              id_shops.add(foodsShopItem.food.shopId);
-            }
-            if (itemsByIdShops[foodsShopItem.food.shopId] == null) {
-                itemsByIdShops[foodsShopItem.food.shopId] = [];
-            }
-            itemsByIdShops[foodsShopItem.food.shopId]!.add(orderItem);
-         }
+    cartItems = Provider.of<CartProvider>(context, listen: false).getCartItems();
+    for (CartItem cartItem in cartItems) {
+      for (FoodCartItem foodsShopItem in cartItem.items) {
+        OrderItem orderItem = OrderItem(
+          foodId: foodsShopItem.food.id,
+          quantity: foodsShopItem.quantity,
+          price: foodsShopItem.food.price,
+          id: 0,
+        );
+        if (!id_shops.contains(foodsShopItem.food.shopId)) {
+          id_shops.add(foodsShopItem.food.shopId);
+        }
+        if (itemsByIdShops[foodsShopItem.food.shopId] == null) {
+          itemsByIdShops[foodsShopItem.food.shopId] = [];
+        }
+        itemsByIdShops[foodsShopItem.food.shopId]!.add(orderItem);
+      }
     }
   }
 
@@ -80,6 +75,7 @@ class _OrderScreenState extends State<OrderScreen> {
       );
       return;
     }
+    // Handle order processing here.
   }
 
   @override
@@ -88,226 +84,186 @@ class _OrderScreenState extends State<OrderScreen> {
       appBar: AppBar(
         title: const Text('Đơn hàng'),
       ),
-      bottomNavigationBar:
-          OrderScreenBottomAppBar(
-          totalPrices: context.watch<CartProvider>().getTotalPrice(),
-          paymentMethod: _paymentMethod,
-          itemsByIdShops: itemsByIdShops,
-          note: note,
-          idAddress: id_address,
-          ),
+      bottomNavigationBar: OrderScreenBottomAppBar(
+        totalPrices: context.watch<CartProvider>().getTotalPrice(),
+        paymentMethod: _paymentMethod,
+        itemsByIdShops: itemsByIdShops,
+        note: note,
+        idAddress: id_address,
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              // Address Section
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, top: 8.0, bottom: 8.0, right: 15.0),
-                          child: Text(
-                            "Thông tin giao hàng",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.surface,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      ListTile(
-                        title: Text(primaryAddress.name),
-                        subtitle: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(primaryAddress.phone),
-                              Text(primaryAddress.address),
-                            ]),
-                        trailing: TextButton(
-                          onPressed: () {
-                            // Navigate to address screen
-                          },
-                          child: const Text('Thay đổi'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              
-              // Order Items Section
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, top: 8.0, bottom: 8.0, right: 15.0),
-                          child: Text(
-                            "Danh sách món",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.surface,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Flexible(
-                        child: Column(
-                          children: itemsByIdShops.keys.map((shopId) {
-                            final orderItems = itemsByIdShops[shopId]!;
-                            final totalPrice = orderItems.fold(
-                                0.0, (sum, item) => sum + (item.price * item.quantity));
+              buildAddressSection(),
+              buildOrderItemsSection(),
+              buildNoteSection(),
+              buildPaymentMethodSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-                            return Card(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      'Shop $shopId',
-                                      style: TextStyle(fontWeight: FontWeight.bold),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    ...orderItems.map((orderItem) {
-                                      return ListTile(
-                                        leading: Icon(Icons.food_bank), 
-                                        title: Text('Món: ${orderItem.foodId}'),
-                                        subtitle: Text('Số lượng: ${orderItem.quantity}'),
-                                        trailing: Text('Giá: ${orderItem.price * orderItem.quantity}'),
-                                      );
-                                    }).toList(),
-                                    const SizedBox(height: 10),
-                                    Text('Tổng cộng: $totalPrice'),
-                                  ],
-                                ),
-                              ),
+  Widget buildAddressSection() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            buildSectionTitle("Thông tin giao hàng"),
+            ListTile(
+              title: Text(primaryAddress?.name ?? "Chưa có địa chỉ"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(primaryAddress?.phone ?? ""),
+                  Text(primaryAddress?.address ?? ""),
+                ],
+              ),
+              trailing: TextButton(
+                onPressed: () {
+                  // Navigate to address screen
+                  Navigator.pushNamed(context, AppRoutes.chooseAddress);
+                },
+                child: const Text('Thay đổi'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildOrderItemsSection() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            buildSectionTitle("Danh sách đơn"),
+            const SizedBox(height: 10),
+            ListView.builder(
+              shrinkWrap: true, // Set shrinkWrap to true to prevent ListView from taking too much space
+              physics: NeverScrollableScrollPhysics(), // Disable scrolling for this ListView since SingleChildScrollView handles scrolling
+              itemCount: cartItems.length,
+              itemBuilder: (context, index) {
+                final cartItem = cartItems[index];
+                final totalPrice = cartItem.totalPrice;
+
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          '${cartItem.shopName}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        Column(
+                          children: cartItem.items.map((orderItem) {
+                            return ListTile(
+                              leading: Icon(Icons.food_bank),
+                              title: Text('Món: ${orderItem.food.name}'),
+                              subtitle: Text('Số lượng: ${orderItem.quantity}'),
+                              trailing: Text('Giá: ${orderItem.totalPrice}'),
                             );
                           }).toList(),
                         ),
-                      ),
-                    ],
+                        const SizedBox(height: 10),
+                        Text('Tổng cộng: $totalPrice'),
+                      ],
+                    ),
                   ),
-                ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildNoteSection() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            buildSectionTitle("Ghi chú"),
+            const SizedBox(height: 10),
+            TextField(
+              onChanged: (value) => note = value,
+              decoration: const InputDecoration(
+                hintText: 'Nhập ghi chú',
+                filled: true,
               ),
-              
-              // Note Section
-              const SizedBox(height: 16),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, top: 8.0, bottom: 8.0, right: 15.0),
-                          child: Text(
-                            "Ghi chú",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.surface,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextField(
-                        onChanged: (value) => note = value,
-                        decoration: const InputDecoration(
-                          hintText: 'Nhập ghi chú',
-                          filled: true,
-                        ),
-                      ),
-                    ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildPaymentMethodSection() {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            buildSectionTitle("Phương thức thanh toán"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: const Text('Thanh toán khi nhận hàng'),
+                    leading: Radio<int>(
+                      value: PaymentMethod.COD,
+                      groupValue: _paymentMethod,
+                      onChanged: (value) =>
+                          setState(() => _paymentMethod = value!),
+                    ),
                   ),
-                ),
-              ),
-              
-              // Payment Method Section
-              const SizedBox(height: 16),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, top: 8.0, bottom: 8.0, right: 15.0),
-                          child: Text(
-                            "Phương thức thanh toán",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.surface,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            ListTile(
-                              title: const Text('Thanh toán khi nhận hàng'),
-                              leading: Radio<int>(
-                                value: PaymentMethod.COD,
-                                groupValue: _paymentMethod,
-                                onChanged: (value) =>
-                                    setState(() => _paymentMethod = value!),
-                              ),
-                            ),
-                            ListTile(
-                              title: const Text('VNPAY'),
-                              leading: Radio<int>(
-                                value: PaymentMethod.VNPAY,
-                                groupValue: _paymentMethod,
-                                onChanged: (value) =>
-                                    setState(() => _paymentMethod = value!),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  ListTile(
+                    title: const Text('VNPAY'),
+                    leading: Radio<int>(
+                      value: PaymentMethod.VNPAY,
+                      groupValue: _paymentMethod,
+                      onChanged: (value) =>
+                          setState(() => _paymentMethod = value!),
+                    ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 15.0, top: 8.0, bottom: 8.0, right: 15.0,
+        ),
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.surface,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
